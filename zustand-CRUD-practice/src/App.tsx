f914829143de5a09f0af26bc030615c273bd6580
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-12 05:38:40 +0000

    ok no we've added the updated functionality to complete the CRUD operations using zustand

--- zustand-CRUD-practice/src/App.tsx
+++ zustand-CRUD-practice/src/App.tsx
@@ -6,10 +6,20 @@ const AddTodoSection = () => {
   const [todoItem, setTodoItem] = useState('');
 
   return (
-    <div>
-      <input onChange={(e) => setTodoItem(e.target.value)} value={todoItem} type="text" />
+    <div className="flex flex-row gap-2">
+      <input 
+        className="border-2 border-gray-300 p-2 rounded-md"
+        onChange={(e) => setTodoItem(e.target.value)} 
+        value={todoItem} 
+        type="text" 
+      />
 
-      <button onClick={() => addTodo(todoItem)}>Add todo</button>
+      <button 
+        className="bg-blue-500 text-white p-2 rounded-md border-2 border-blue-500"
+        onClick={() => addTodo(todoItem)}
+      >
+        Add todo
+      </button>
     </div>
   )
 }
@@ -17,10 +27,14 @@ const AddTodoSection = () => {
 function App() {
   const todoItems = useTodoStore((state) => state.todoItems);
   const removeTodo = useTodoStore((state) => state.removeTodo);
+  const editTodo = useTodoStore((state) => state.editTodo);
+  const saveTodo = useTodoStore((state) => state.saveTodo);
+
+  const [todoTitle, setTodoTitle] = useState('');
 
   return (
-    <main>
-      <section>Hello, Vite</section>
+    <main className="mt-32 flex flex-col w-6/12 mx-auto gap-4">
+      <h1 className="text-4xl font-bold mb-4">zustand CRUD practice</h1>
 
       <section>
         <AddTodoSection />
@@ -28,7 +42,38 @@ function App() {
 
       <section>
         {todoItems.map((todoItem) => {
-          return <p>{todoItem.title} <span onClick={() => removeTodo(todoItem)}>Remove TODO</span></p>
+          return (
+            <div key={todoItem.id} className="flex flex-row items-center gap-4 mb-4 last:mb-0">
+              {!todoItem.isEditable ? <p>{todoItem.title}</p> : (
+                <input
+                  className="border-2 border-gray-300 p-2 rounded-md"
+                  onChange={(e) => setTodoTitle(e.target.value)}
+                  defaultValue={todoItem.title}
+                  type="text"
+                />
+              )}
+              
+              {!todoItem.isEditable ? (
+                <span 
+                className="bg-yellow-500 text-white p-2 rounded-md cursor-pointer"
+                onClick={() => editTodo(todoItem)}>
+                  edit
+                </span>
+              ) : (
+                <span 
+                className="bg-green-500 text-white p-2 rounded-md cursor-pointer"
+                onClick={() => saveTodo(todoTitle, todoItem)}>
+                  save
+                </span>
+              )}
+
+              <span 
+                className="bg-red-500 text-white p-2 rounded-md cursor-pointer"
+                onClick={() => removeTodo(todoItem)}>
+                  remove
+              </span>
+            </div>
+          )
         })}
       </section>
     </main>
--- zustand-CRUD-practice/src/hooks/useTodoStore.tsx
+++ zustand-CRUD-practice/src/hooks/useTodoStore.tsx
@@ -4,11 +4,14 @@ import { v4 as uuidv4 } from 'uuid';
 type Todo = {
   id: string;
   title: string;
+  isEditable?: boolean;
 };
 
 type TodoStateType = {
   todoItems: Todo[];
   addTodo: (todoTitle: string) => void;
+  editTodo: (todoItem: Todo) => void;
+  saveTodo: (todoTitle: string, todoItem: Todo) => void;
   removeTodo: (todoItem: Todo) => void;
 };
 
@@ -20,6 +23,46 @@ const setAddTodo = (set: any) => (todoTitle: string) => {
 
   set((state: TodoStateType) => ({
     todoItems: [...state.todoItems, newTodoItem],
+  }));
+};
+
+const setEditTodo = (set: any) => (todoItem: Todo) => {
+  const newTodoItem = {
+    ...todoItem,
+    isEditable: true,
+  };
+
+  set((state: TodoStateType) => ({
+    todoItems: state.todoItems.map((_todoItem) => {
+      if (_todoItem.id === todoItem.id) {
+        debugger
+        return {
+          ...newTodoItem,
+        };
+      } else {
+        return _todoItem;
+      }
+    }),
+  }));
+};
+
+const setSaveTodo = (set: any) => (todoTitle: string, todoItem: Todo) => {
+  const updatedTodoItem = {
+    ...todoItem,
+    title: todoTitle,
+    isEditable: false,
+  };
+
+  set((state: TodoStateType) => ({
+    todoItems: state.todoItems.map((_todoItem) => {
+      if (_todoItem.id === todoItem.id) {
+        return {
+          ...updatedTodoItem,
+        };
+      } else {
+        return _todoItem;
+      }
+    }),
   }));
 };
 
@@ -41,5 +84,7 @@ const initialTodoItems: Todo[] = [
 export const useTodoStore = create<TodoStateType>((set) => ({
   todoItems: initialTodoItems,
   addTodo: setAddTodo(set),
+  editTodo: setEditTodo(set),
+  saveTodo: setSaveTodo(set),
   removeTodo: setRemoveTodo(set),
 }));

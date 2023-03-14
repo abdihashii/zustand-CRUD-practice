967ca49deb73456c5ae2e3d0d365fdcd20ac2789
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-03-14 12:49:22 -0500

    making store even simpler and adding finishedItems API

--- zustand-CRUD-practice/src/hooks/useTodoStore.tsx
+++ zustand-CRUD-practice/src/hooks/useTodoStore.tsx
@@ -1,73 +1,19 @@
-import { create } from "zustand";
-import { v4 as uuidv4 } from "uuid";
-
-type Todo = {
-  id: string;
-  title: string;
-  isEditable?: boolean;
-};
-
-type TodoStateType = {
-  todoItems: Todo[];
-  addTodo: (todoTitle: string) => void;
-  editTodo: (todoItem: Todo) => void;
-  saveTodo: (todoTitle: string, todoItem: Todo) => void;
-  removeTodo: (todoItem: Todo) => void;
-};
-
-const setEditTodo = (set: any) => (todoItem: Todo) => {
-  const newTodoItem = {
-    ...todoItem,
-    isEditable: true,
-  };
-
-  set((state: TodoStateType) => ({
-    todoItems: state.todoItems.map((_todoItem) => {
-      if (_todoItem.id === todoItem.id) {
-        return {
-          ...newTodoItem,
-        };
-      } else {
-        return _todoItem;
-      }
-    }),
-  }));
-};
-
-const setSaveTodo = (set: any) => (todoTitle: string, todoItem: Todo) => {
-  const updatedTodoItem = {
-    ...todoItem,
-    title: todoTitle,
-    isEditable: false,
-  };
-
-  set((state: TodoStateType) => ({
-    todoItems: state.todoItems.map((_todoItem) => {
-      if (_todoItem.id === todoItem.id) {
-        return {
-          ...updatedTodoItem,
-        };
-      } else {
-        return _todoItem;
-      }
-    }),
-  }));
-};
-
-const removeTodo = (todoItems: Todo[], todoItem: Todo) =>
-  todoItems.filter((_todoItem) => _todoItem.id !== todoItem.id);
+import { create } from 'zustand';
+import { v4 as uuidv4 } from 'uuid';
+import { Todo, TodoStateType } from '../types/todoTypes';
 
 const initialTodoItems: Todo[] = [
   {
-    id: "1",
-    title: "first Todo",
+    id: '1',
+    title: 'first Todo',
   },
 ];
 
 export const useTodoStore = create<TodoStateType>((set) => ({
   todoItems: initialTodoItems,
-  addTodo: (todoTitle: string) =>
-    set((state: TodoStateType) => ({
+  finishedItems: [],
+  addTodo: (todoTitle) =>
+    set((state) => ({
       ...state,
       todoItems: [
         ...state.todoItems,
@@ -77,11 +23,116 @@ export const useTodoStore = create<TodoStateType>((set) => ({
         },
       ],
     })),
-  editTodo: setEditTodo(set),
-  saveTodo: setSaveTodo(set),
-  removeTodo: (todoItem: Todo) =>
-    set((state: TodoStateType) => ({
+  // editTodo: setEditTodo(set),
+  editTodo: (todoItem) => {
+    set((state) => {
+      const newTodoItem = {
+        ...todoItem,
+        isEditable: true,
+      };
+
+      const newTodoItems = state.todoItems.map((_todoItem) => {
+        if (_todoItem.id === todoItem.id) {
+          return {
+            ...newTodoItem,
+          };
+        } else {
+          return _todoItem;
+        }
+      });
+
+      return {
+        ...state,
+        todoItems: newTodoItems,
+      };
+    });
+  },
+  // saveTodo: setSaveTodo(set),
+  saveTodo: (todoItemTitle, todoItem) => {
+    set((state) => {
+      const newTodoItem = {
+        ...todoItem,
+        title: todoItemTitle,
+        isEditable: false,
+      };
+
+      const newTodoItems = state.todoItems.map((_todoItem) => {
+        if (_todoItem.id === todoItem.id) {
+          return {
+            ...newTodoItem,
+          };
+        } else {
+          return _todoItem;
+        }
+      });
+
+      return {
+        ...state,
+        todoItems: newTodoItems,
+      };
+    });
+  },
+  removeTodo: (todoItem) =>
+    set((state) => ({
       ...state,
-      todoItems: removeTodo(state.todoItems, todoItem),
+      todoItems: state.todoItems.filter(
+        (_todoItem) => _todoItem.id !== todoItem.id,
+      ),
     })),
+  finishTodo: (todoItem, index) =>
+    set((state) => {
+      const newTodoItems = [...state.todoItems];
+
+      newTodoItems.splice(index, 1);
+
+      const newFinishedItems = [...state.finishedItems, todoItem];
+
+      return {
+        ...state,
+        todoItems: newTodoItems,
+        finishedItems: newFinishedItems,
+      };
+    }),
 }));
+
+// const setEditTodo = (set: any) => (todoItem: Todo) => {
+//   const newTodoItem = {
+//     ...todoItem,
+//     isEditable: true,
+//   };
+
+//   set((state: TodoStateType) => ({
+//     todoItems: state.todoItems.map((_todoItem) => {
+//       if (_todoItem.id === todoItem.id) {
+//         return {
+//           ...newTodoItem,
+//         };
+//       } else {
+//         return _todoItem;
+//       }
+//     }),
+//   }));
+// };
+
+// const setSaveTodo = (set: any) => (todoTitle: string, todoItem: Todo) => {
+//   const updatedTodoItem = {
+//     ...todoItem,
+//     title: todoTitle,
+//     isEditable: false,
+//   };
+
+//   set((state: TodoStateType) => ({
+//     todoItems: state.todoItems.map((_todoItem) => {
+//       if (_todoItem.id === todoItem.id) {
+//         return {
+//           ...updatedTodoItem,
+//         };
+//       } else {
+//         return _todoItem;
+//       }
+//     }),
+//   }));
+// };
+
+// const removeTodo = (todoItems: Todo[], todoItem: Todo) =>
+//   todoItems.filter((_todoItem) => _todoItem.id !== todoItem.id);
